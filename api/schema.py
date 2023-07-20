c94b945660c6b0c9c9ba377e2fa670ed7773686d
vanesagh <vanesag@chalmers.se>
2023-07-20 11:55:31 -0600

    First commit

---
+++ api/schema.py
@@ -0,0 +1,24 @@
+import graphene
+
+from graphene_django import DjangoObjectType, DjangoListField
+from .models import Product
+
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+class Query(graphene.ObjectType):
+    all_products = graphene.List(ProductType)
+    product = graphene.Field(ProductType, id=graphene.Int())
+
+    def resolve_all_products(self, info, **kwargs):
+        return Product.objects.all()
+
+    def resolve_product(self, info, id):
+        return Product.objects.get(pk=id)
+
+
+schema = graphene.Schema(query=Query)
