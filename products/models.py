c81a67bf516dc128b058e3ff6b50895da91e0cc7
vanesagh <vanesag@chalmers.se>
2023-07-26 19:04:04 -0600

    Minor changes

--- products/models.py
+++ products/models.py
@@ -1,20 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
 class Product(models.Model):
     CATEGORIES = [
-        ('pan','Pan'),
-        ('pastel','Pastel'),
-        ('galletas','Galletas'),
-        ('pays','Pays'),
+        ('pan', 'Pan'),
+        ('pastel', 'Pastel'),
+        ('galletas', 'Galletas'),
+        ('pays', 'Pays'),
     ]
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    category = models.CharField(max_length=10, choices=CATEGORIES,null=True)
-
+    category = models.CharField(max_length=10, choices=CATEGORIES, null=True)
 
     def __str__(self):
         return self.name
-
-
