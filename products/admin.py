bedcbe9f8c29c18642c810e48a74dd66fce0f0f5
vanesagh <vanesag@chalmers.se>
2023-08-08 18:11:00 -0600

    Modified Product models, added Presentation  and Category

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product
+from .models import Product, Presentation, Category
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Presentation)
+class PresentationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- products/models.py
+++ products/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Presentation(models.Model):
+    name = models.CharField(max_length=200)
+    unique_characteristics = models.JSONField(blank=True, null=True)
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
 class Product(models.Model):
-    CATEGORIES = [
-        ('pan', 'Pan'),
-        ('pastel', 'Pastel'),
-        ('galletas', 'Galletas'),
-        ('pays', 'Pays'),
-    ]
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    category = models.CharField(max_length=10, choices=CATEGORIES, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    presentation = models.ForeignKey(Presentation, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
